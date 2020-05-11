fa8d73661ee0e667b9e69f56b41aa9a152ed5fd9
tynail <marc4283@hotmail.com>
2020-05-11 11:43:21 -0400

    Move styles out and add contants file

--- src/Palette.js
+++ src/Palette.js
@@ -3,8 +3,7 @@ import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
 import { withStyles } from "@material-ui/styles";
-import styles from './styles/PaletteStyles';
-
+import styles from "./styles/PaletteStyles";
 
 class Palette extends Component {
   constructor(props) {
