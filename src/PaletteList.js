c08640161ec0c77d4dca38ead8c9a39e60e51e6e
tynail <marc4283@hotmail.com>
2020-05-14 13:00:39 -0400

    Refactor with PureComponent

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -28,6 +28,7 @@ class PaletteList extends Component {
     this.openDialog = this.openDialog.bind(this);
     this.closeDialog = this.closeDialog.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
   }
   openDialog(id) {
     this.setState({ openDeleteDialog: true, deletingId: id });
@@ -60,7 +61,7 @@ class PaletteList extends Component {
                   {...palette}
                   id={palette.id}
                   key={palette.id}
-                  handleClick={() => this.goToPalette(palette.id)}
+                  goToPalette={this.goToPalette}
                   // handleDelete={deletePalette}
                   openDialog={this.openDialog}
                 />
